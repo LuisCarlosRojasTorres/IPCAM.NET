75170553702284efa33839a8c733217d43ae7624
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2024-01-11 10:44:34 -0300

    Adding diagrams

---
+++ IpCam/MjpegStreamState.cs
@@ -0,0 +1,15 @@
+﻿namespace IpCam
+{
+    using System;
+
+    /// <summary>
+    /// Video source interface.
+    /// </summary>
+    /// 
+    /// <remarks>The interface describes common methods for different type of video sources.</remarks>
+    /// 
+    public enum MjpegStreamState
+    {
+        
+    }
+}
